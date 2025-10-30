a560ea578ef43f301c7bcaf28304db49cfc9ae8b
Mathieu ACKERMANN <116433624+Floupper@users.noreply.github.com>
2025-10-30 15:55:10 +0100

    Update Docker image name in build.yml and fix database configuration in config.js

--- api/config.js
+++ api/config.js
@@ -1,11 +1,10 @@
-module.exports =  {
-    ACCESS_TOKEN_SECRET : "EMMA123",
-    BDD : {
-    "host" :"dpg-d40798bipnbc73cf4njg-a.oregon-postgres.render.com",
-    "port" : "5432",
-    "user" : "cnam_qnjc_user",
-    "password" : "FOYjmLxOmqwdnfGPhMO4R8eZpErPzVXQ",
-    "bdname" :"cnam_qnjc" 
-    }
-}
-
+module.exports = {
+  ACCESS_TOKEN_SECRET: "EMMA123",
+  BDD: {
+    host: "dpg-d41mvaogjchc73a0pkkg-a.oregon-postgres.render.com",
+    port: "5432",
+    user: "pollutions_rlf2_user",
+    password: "E0jRgpirpbvMkIYpuT6ZVjBTMktxOrP0",
+    bdname: "pollutions_rlf2",
+  },
+};
